11abffeae1d52ef777dcbc7e4d79e9ad4813d8c9
unknown <rocioarrebola95@hotmail.com>
2015-03-28 22:51:27 +0100

    DoublyLinkedList.java modified

--- DoublyLinkedList/src/main/java/IIS/DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/src/main/java/IIS/DoublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,37 @@
 package IIS.DoublyLinkedList;
 
-public class DoublyLinkedList {
-
+public class DoublyLinkedList <E>{
+	
+	public class DoublyLinkedNode<T>{
+		DoublyLinkedNode<T> prev;
+		DoublyLinkedNode<T> next;
+		T data;
+		
+		public DoublyLinkedNode (T data){
+			prev = null;
+			next = null;
+			this.data = data;
+		}
+		
+		public DoublyLinkedNode (DoublyLinkedNode<T> prev, DoublyLinkedNode<T> next, T data){
+			this.prev = prev;
+			this.next = next;
+			this.data = data;
+		}
+	}
+	
+	DoublyLinkedNode<E> firstNode;
+	DoublyLinkedNode<E> lastNode;
+	
+	public DoublyLinkedList (DoublyLinkedNode<E> node){
+		firstNode = node;
+		lastNode = null;
+	}
+	
+	public DoublyLinkedList (DoublyLinkedNode<E> first, DoublyLinkedNode<E> last){
+		firstNode = first;
+		lastNode = last;
+	}
+	
+	
 }
