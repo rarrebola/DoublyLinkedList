cf5aa397eae99b1a0a779663cace37bb680967f4
unknown <rocioarrebola95@hotmail.com>
2015-05-23 11:45:20 +0200

    Clase DoublyLinkedNodeException añadida

---
+++ DoublyLinkedList/src/main/java/IIS/DoublyLinkedList/DoublyLinkedNodeException.java
@@ -0,0 +1,14 @@
+package iis.doublylinkedlist;
+
+public class DoublyLinkedNodeException extends RuntimeException {
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public DoublyLinkedNodeException(String msj){
+    super(msj);
+  }
+
+}
